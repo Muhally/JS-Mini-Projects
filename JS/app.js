c0ef50232e1bf16f1a44992dceeae5a83cf89187
Muhally <muhammadalidehghanian@gmail.com>
2022-08-29 17:24:35 +0430

    FIX#02

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,23 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"]
+
+const button = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+button.addEventListener('click', () => {
+
+    // Getting a random number between 0 , 3
+
+    const randomNumber = getRandomNumber()
+
+    document.body.style.backgroundColor = colors[randomNumber]
+
+    color.textContent = colors[randomNumber]
+    // textContent is a property!
+
+})
+
+
+function getRandomNumber (){
+
+    return Math.floor(Math.random() * colors.length)
+}
